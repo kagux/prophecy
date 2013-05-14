8bfaa78528af7d6c9d1ed603a0f48c37e9957340
Boris Mikhaylov <boris@exoclick.com>
2013-05-14 16:10:52 +0200

    cleaned up logical not token spec

--- spec/Prophecy/Argument/Token/LogicalNotTokenSpec.php
+++ spec/Prophecy/Argument/Token/LogicalNotTokenSpec.php
@@ -3,22 +3,28 @@
 namespace spec\Prophecy\Argument\Token;
 
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument\Token\TokenInterface;
 
 class LogicalNotTokenSpec extends ObjectBehavior
 {
+
+    /**
+     * @param \Prophecy\Argument\Token\TokenInterface $token
+     */
+    function let($token)
+    {
+        $this->beConstructedWith($token);
+    }
+
     function it_implements_TokenInterface()
     {
         $this->beConstructedWith('value');
         $this->shouldBeAnInstanceOf('Prophecy\Argument\Token\TokenInterface');
     }
 
-    /**
-     * @param \Prophecy\Argument\Token\TokenInterface $token
-     */
-    function it_has_simple_string_representation($token)
+    function it_has_simple_string_representation(TokenInterface $token)
     {
         $token->__toString()->willReturn('value');
-        $this->beConstructedWith($token);
         $this->__toString()->shouldBe('not(value)');
     }
 
@@ -30,43 +36,27 @@ class LogicalNotTokenSpec extends ObjectBehavior
         $this->__toString()->shouldBe('not(exact("value"))');
     }
 
-    /**
-     * @param \Prophecy\Argument\Token\TokenInterface $token
-     */
-    function it_scores_8_if_preset_token_does_not_match_the_argument($token)
+    function it_scores_8_if_preset_token_does_not_match_the_argument(TokenInterface $token)
     {
         $token->scoreArgument('argument')->willReturn(false);
-        $this->beConstructedWith($token);
         $this->scoreArgument('argument')->shouldBe(8);
     }
 
-    /**
-     * @param \Prophecy\Argument\Token\TokenInterface $token
-     */
-    function it_does_not_score_if_preset_token_matches_argument($token)
+    function it_does_not_score_if_preset_token_matches_argument(TokenInterface $token)
     {
         $token->scoreArgument('argument')->willReturn(5);
-        $this->beConstructedWith($token);
         $this->scoreArgument('argument')->shouldBe(false);
     }
 
-    /**
-     * @param \Prophecy\Argument\Token\TokenInterface $token
-     */
-    function it_is_last_if_preset_token_is_last($token)
+    function it_is_last_if_preset_token_is_last(TokenInterface $token)
     {
         $token->isLast()->willReturn(true);
-        $this->beConstructedWith($token);
         $this->isLast()->shouldBe(true);
     }
 
-    /**
-     * @param \Prophecy\Argument\Token\TokenInterface $token
-     */
-    function it_is_not_last_if_preset_token_is_not_last($token)
+    function it_is_not_last_if_preset_token_is_not_last(TokenInterface $token)
     {
         $token->isLast()->willReturn(false);
-        $this->beConstructedWith($token);
         $this->isLast()->shouldBe(false);
     }
 
